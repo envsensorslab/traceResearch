ae46c7160ff58eff485e25b9945abec0bc824dc8
fizzshaikh <shaikh.f@husky.neu.edu>
2017-02-09 19:10:16 -0500

    Pump sequence

---
+++ Operation Mode/Timers/Timers/Timers.ino
@@ -0,0 +1,25 @@
+//pumping loop
+//A3 is pump enable 
+//A2 is pump direction
+
+int pumpDirectionPin = A2;
+int pumpEnablePin = A3;
+
+void setup() {
+//set pin direction forward (set to high)
+  pinMode(pumpDirectionPin, OUTPUT);
+  
+//start timer 
+
+//run pump sequence
+
+//when timer hits value, reverse pin direction 
+
+//reverse pump sequence and run (set to low)
+
+//timer ends 
+}
+
+void loop() {
+
+}
