51af0690fd18a5f0cea161e37f6a5c6010f668c8
fizzshaikh <shaikh.f@husky.neu.edu>
2017-02-16 16:51:27 -0500

    Working timers
    Timer sequence working, code sent to Mueller to verify

--- Operation Mode/Timers/Timers/Timers.ino
+++ Operation Mode/Timers/Timers/Timers.ino
@@ -3,11 +3,10 @@
 //A2 is pump direction
 
 #include <TimerOne.h>
+//these LEDs are for testing purposes
 int pumpDirection = A2; //green LED
 int pumpEnable= A3; //blue LED
-//these LEDs are for testing purposes
-int pumpDirectionLED = 8;
-int pumpEnableLED = 9;
+
 
 int wait=0;
 
@@ -15,28 +14,30 @@ int wait=0;
 long startTime;
 long elapsedTime;
 
+long forward_time  = 4000000;
+long backward_time = 4000000;
+
 
 
 void setup() {
   Serial.begin(9600);
   pinMode(pumpDirection, OUTPUT);
   pinMode(pumpEnable, OUTPUT);
-  Timer1.initialize(100000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
-  Timer1.attachInterrupt( direction_timer );
-
- /* noInterrupts();
-  //sets TCCR1A and TCCR1B registers to 0
+  // Timer1.initialize(4000000); // set a timer of length 4000000 microseconds (or 4 sec)
+  Timer1.attachInterrupt(isrDirection);
+  /*noInterrupts();
+  //disables interrupts
   TCCR1A = 0;
   TCCR1B = 0;
-  TCNT1 = 0x0BDC;
+  
   //prescale the timer to 4s
-  //OCR1A = 31250;         //sets compare match register to desired timer count
-  TCCR1B|= (1 << WGM12); //sets TCCR1B timer period to 256 Hz
-  TCCR1B|= (1 << CS12);
-  TIMSK1|= (1 << OCIE1A);
-
-  interrupts();
+  int  timer1_counter = 15624;
+  TCNT1 = timer1_counter;
+  TCCR1B |= (1 << CS12); //256 prescaler 
+  TIMSK1 |= (1 << TOIE1); //enable timer overflow interrupt 
+  interrupts(); //reenable interrupts 
   */
+  
 }
 
 void loop() {
@@ -49,6 +50,7 @@ void loop() {
   } //while loop closed
   pump_sequence_backward();
   Serial.println("Pump sequence complete");
+  Serial.println("");
   
   
 
@@ -56,40 +58,33 @@ void loop() {
 
 void pump_sequence_forward(){
   //set pin direction forward (set to high)
+  Timer1.initialize(forward_time);  //initialize timer
   Serial.println("Set pin direction forward");
   digitalWrite(A2, HIGH);
-  Serial.println("Start delay");
-  delay(100);
-  Serial.println("End delay");
+  // Serial.println("Start delay");
+  delay(500);
+  //Serial.println("End delay");
   
   
 //start timer 
   //RTC timer 
-  Serial.println("Start timer");
-  startTime = millis();
+  //Serial.println("Start timer");
+  //startTime = millis();
   
   
 //run pump sequence
   //set pump enable to high
   Serial.println("Set pump enable high");
   digitalWrite(A3, HIGH);
-  delay(100);
-  
-
-
-//reverse pump sequence and run 
-  //set pump enable to high
-  Serial.println("Set pump enable high");
-  digitalWrite(A3, HIGH);
-  delay(100);
+  delay(500);
   
 
 //timer ends 
   //set pump enable to low
-  Serial.println("Set pump enable low"); 
-  digitalWrite(A3, LOW);
+  //Serial.println("Set pump enable low"); 
+  //digitalWrite(A3, LOW);
 
-  Serial.println(elapsedTime);
+  //Serial.println(elapsedTime);
   Serial.println();
 
 }
@@ -98,26 +93,33 @@ void pump_sequence_backward(){
   //when timer hits value, reverse pin direction
   //set direction pin to low  
   //set pump enable to low
-  direction_timer();
+
+  Serial.println("Read pressure and temp here");
+  //sensor_inputs(); //call this subfunction to read pressure and temperature here
+
+ 
+  Timer1.initialize(backward_time);  //initialize timer
   Serial.println("Now running backward pump sequence");
   Serial.println("Set direction pin low");
   digitalWrite(A2, LOW);
-  Serial.println("Set pump enable pin low");
-  digitalWrite(A3, LOW);
-  Serial.println("Read pressure and temp here");
-  //sensor_inputs(); //call this subfunction to read pressure and temperature here
-  elapsedTime = millis()-startTime;
-  Serial.println("Start timing");
-  delay(100);
+   Serial.println("Set pump enable pin high- start pumping");
+  digitalWrite(A3, HIGH);
+  
+  
+  //elapsedTime = millis()-startTime;
+  //Serial.println("Start timing");
+  delay(500);
   Serial.println();
   
   
 }
-void direction_timer(){
+void isrDirection(){
   Serial.println("Interrupt starting");
-  digitalWrite(A2, digitalRead(A2)^1);
   wait = 1;
-  Serial.println("Interrupt occurred");
+  Serial.println("Set pump enable pin low- stop pumping");
+  digitalWrite(A3, LOW);
+  Timer1.stop();
+  Serial.println("Interrupt finished");
 }
 
 
