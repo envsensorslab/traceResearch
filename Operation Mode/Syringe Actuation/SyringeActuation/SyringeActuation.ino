11bdfed65b91072cb6c3e09e462176758b9eb953
Tiusty <awagud12@gmail.com>
2017-02-22 12:54:23 -0500

    Got matrix working

--- Operation Mode/Syringe Actuation/SyringeActuation/SyringeActuation.ino
+++ Operation Mode/Syringe Actuation/SyringeActuation/SyringeActuation.ino
@@ -1,5 +1,3 @@
-void setup() {
-/*
 #define syringePin1 2
 #define syringePin2 3
 #define syringePin3 4
@@ -8,16 +6,21 @@ void setup() {
 #define syringePin6 7
 #define syringePin7 8
 #define syringePin8 9
-*/
 
-#define syringePin1 22
-#define syringePin2 24
-#define syringePin3 26
-#define syringePin4 28
-#define syringePin5 30
-#define syringePin6 32
-#define syringePin7 34
-#define syringePin8 36
+int delayTime = 1000;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  for (int i =0; i< 8; i++)
+  {
+    Serial.println("hi");
+    digitalWrite(i+2, LOW);
+    pinMode(i+2, OUTPUT);
+  }
+  delay(1000);
 }
 
 void loop() {
@@ -28,22 +31,22 @@ digitalWrite(syringePin1,HIGH);
 
 //syringe 1
 digitalWrite(syringePin5,HIGH);
-delay(1000);
+delay(delayTime);
 digitalWrite(syringePin5,LOW);
 
 //syringe 2
 digitalWrite(syringePin6,HIGH);
-delay(1000);
+delay(delayTime);
 digitalWrite(syringePin6,LOW);
 
 //syringe 3
 digitalWrite(syringePin7,HIGH);
-delay(1000);
+delay(delayTime);
 digitalWrite(syringePin7,LOW);
 
 //syringe 4
 digitalWrite(syringePin8,HIGH);
-delay(1000);
+delay(delayTime);
 digitalWrite(syringePin8,LOW);
 
 digitalWrite(syringePin1,LOW);
@@ -54,22 +57,26 @@ digitalWrite(syringePin2, HIGH);
 
 //syringe 5
 digitalWrite(syringePin5,HIGH);
-delay(1000);
+delay(delayTime);
 digitalWrite(syringePin5,LOW);
 
 //syringe 6
 digitalWrite(syringePin6,HIGH);
-delay(1000);
+delay(delayTime);
 digitalWrite(syringePin6,LOW);
 
 //syringe 7
 digitalWrite(syringePin7,HIGH);
-delay(1000);
+delay(delayTime);
 digitalWrite(syringePin7,LOW);
 
 //syringe 8
 digitalWrite(syringePin8, HIGH);
-delay(1000);
+delay(delayTime);
 digitalWrite(syringePin8, LOW);
 
+digitalWrite(syringePin2, LOW);
+
+
+ 
 }
