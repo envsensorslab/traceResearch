8e9f98a7dc5f3e5106a2ef6c4e11725d6d1d30b0
Tiusty <awagud12@gmail.com>
2017-02-23 11:31:45 -0500

    Fixed up pumpSequence

--- Operation Mode/Sample_Sequence/Sample_Sequence.ino
+++ Operation Mode/Sample_Sequence/Sample_Sequence.ino
@@ -8,69 +8,87 @@ int pumpEnable= A3; //blue LED
 
 
 boolean pumpOn = false;
+//pumpForw == true -> forward direction
+//pumpForw == false -> reverse direction
 boolean pumpForw = true;
 
-int forward_time = 5000*2;
+byte forward_flush_time = 5000;
+byte reverse_flush_time = 2000;
 
 
 
 void setup() {
   pinMode(pumpDirection, OUTPUT);
-  pinMode(pumpEnable, OUTPUT);
-  
+  pinMode(pumpEnable, OUTPUT);  
 }
 
 void loop() {
-
-
-  pump_sequence_forward();
- 
-  delay(forward_time);
-  
-  //sensor_inputs(); //call this subfunction to read pressure and temperature here
-  //String output = "";
-  //output = "Rev flush: " + (String)[curr_syringe, pressure, temperature];
-  //LogPrint(SYSTEM, LOG_INFO, output.c_str());                   
-  
-  //sub-function for syringe actuation
-  //syringe_actuation(curr_syringe)
-
-  updatePumpState(true,false);
+  // Start pumping in the forward direction
+  pump_sequence_forward();  
+  // Start pumping in the reverse direction
   pump_sequence_backward();
 }
 
 
-// sub-functions //////////////
+/*
+ * Function: pump_sequence_forware()
+ * 
+ * Description: Starts the pump in the forward direction for the specified amount of time
+ *  from the EEPROM
+ *  
+ *  Relies:
+ *    forward_flush_time: which is a global variable read in from the EEPROM
+ */
 void pump_sequence_forward(){
 
   pumpForw = true;
-
-  digitalWrite(pumpDirection,pumpForw);
-  pumpOn = true;
-  digitalWrite(pumpEnable,pumpOn);
-
+  pumpOn = true;  
+  updatePumpState(pumpOn,pumpForw);
+  delay(forward_flush_time);
   pumpOn= false;
-  digitalWrite(pumpEnable, pumpOn);
+  updatePumpState(pumpOn, pumpForw);
 
 }
 
+/*
+ * Function: pump_sequence_backward()
+ * 
+ * Description: Starts the pum in the reverse direction for the specified amount of time
+ * 
+ * Relies:
+ *  reverse_flush_time: which is a global variable read in from the EEPROM
+ */
 void pump_sequence_backward(){
   //when timer hits value, reverse pin direction
   //set direction pin to low  
   //set pump enable to low
 
   pumpForw = false;
-  digitalWrite(pumpDirection, pumpForw);
   pumpOn = true;
-  digitalWrite(pumpEnable, pumpOn);
-
+  updatePumpState(pumpOn, pumpForw);
+  delay(reverse_flush_time);
   pumpOn= false;
-  digitalWrite(pumpEnable, pumpOn);
+  updatePumpState(pumpOn, pumpForw);
 
   
 
 }
 
+/*
+ * Function: updatePumpState(boolean pumpPower, boolean pumpDLR)
+ * 
+ * Description: Updates the current state of the pump. It is passed in the direction the pump should be 
+ *  and also if the pump should be enabled. 
+ *  
+ *  Arguments:
+ *    boolean pumpPower: True or False to indicate whether the pump enable should occur
+ *    boolean pumpDLR: True or False to indicate the direction the pump should pump
+ *      True -> Forward direction
+ *      False -> Reverse direction
+ *      
+ *  Relies:
+ *    The pin numbers for the pumpDirection and the pumpEnable
+ */
 void updatePumpState(boolean pumpPower, boolean pumpDLR){
   pumpForw = pumpDLR;
   digitalWrite(pumpDirection, pumpForw);
