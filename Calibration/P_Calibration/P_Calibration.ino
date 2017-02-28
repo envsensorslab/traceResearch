c261a249eec6bf5d61a546a59fd3ab2f9ac77aea
Tiusty <awagud12@gmail.com>
2017-02-28 17:14:57 -0500

    Fixed calibration data with new constants etc...

--- Calibration/P_Calibration/P_Calibration.ino
+++ Calibration/P_Calibration/P_Calibration.ino
@@ -10,13 +10,14 @@
 const int pressurePin = A7;    // pressure sensor 
 
 
-int v_power = 9.75;
-const int a = 0.0014705;
-const int b = 0.0002378;
-const int m = 0.199992;
-const int b2 = 0.00386;
+float v_power = 9.75;
+const float a = 0.066466;
+const float b = 0.02523;
+const float m = 0.199992;
+const float b2 = 0.00386;
 
-int sensor_values[10]={44,45,46,47,47,48,49,50,50,51};
+#define SENSOR_SIZE 11
+int sensor_values[SENSOR_SIZE]={0, 2, 4, 6, 8, 10, 12, 60, 80, 90, 100};
 
 int eeAddress=0;
 //depth = (10m/14.57psi)*pressure
@@ -27,13 +28,13 @@ int eeAddress=0;
 void setup(){
 
   Serial.begin(9600);
-   for (int i=0; i<10; i++){
-  Serial.print(sensor_values[i]);
+   for (int i=0; i<SENSOR_SIZE; i++){
+  Serial.println(sensor_values[i]);
    } // closes for loop
 }
 
 void loop(){
-   mV_to_meters();
+   meters_to_mV();
 }
 
 
@@ -41,15 +42,15 @@ void mV_to_meters(){
   //millivolts to meters (v_arduino to depth)
   //given sensor mV
   
-  int pressure;
-  int depth;
-  int p_arduino;
-  int pv_arduino;
-  int pv_sensor;
+  float pressure;
+  float depth;
+  float p_arduino;
+  float pv_arduino;
+  float pv_sensor;
 
   Serial.print("Read mV from array");
   
-  for (int i=0; i<10; i++){
+  for (int i=0; i<SENSOR_SIZE; i++){
     
   p_arduino = sensor_values[i];
   pv_arduino = (p_arduino*5)/1023; //v_arduino to pv_arduino
@@ -72,28 +73,30 @@ void mV_to_meters(){
 void meters_to_mV(){
   //reverse of the above subfunction
   //given depth by client
-  int pressure;
-  int depth;
-  int p_arduino;
-  int pv_arduino;
-  int pv_sensor;
+  float pressure;
+  float depth;
+  float p_arduino;
+  float pv_arduino;
+  float pv_sensor;
 
+  for (int i=0; i<SENSOR_SIZE; i++){
+    pressure = ((sensor_values[i] * 14.57/10) + 14.7);
+    pv_sensor = (v_power/10)*(pressure*m + b2);
+    pv_arduino = pv_sensor*a + b;
+    p_arduino = (pv_arduino*1023)/5;
   
-  pressure = (depth * 14.57/10);
-  pv_sensor = (v_power/10)*(pressure*m + b2);
-  pv_arduino = pv_sensor*a + b;
-  p_arduino = (pv_arduino*1023)/5;
-
-  Serial.println("Depth");
-  Serial.print(depth);
-  Serial.println("Pressure: ");
-  Serial.println(pressure);
-  Serial.print("PV_sensor:  ");
-  Serial.print(pv_sensor);
-  Serial.println("PV_arduino");
-  Serial.print(pv_arduino);
-  Serial.println("P_arduino");
-  Serial.print(p_arduino);
+    
+    Serial.print("Depth: ");
+    Serial.println(sensor_values[i]);
+    Serial.print("Pressure: ");
+    Serial.println(pressure);
+    Serial.print("PV_sensor:  ");
+    Serial.println(pv_sensor);
+    Serial.print("PV_arduino: ");
+    Serial.println(pv_arduino);
+    Serial.print("P_arduino: ");
+    Serial.println(p_arduino);
+  }
  
 } //closes function 
 
