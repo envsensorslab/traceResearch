5108f6625cb09a403700f4a1e20a49f07da83cfe
fizzshaikh <shaikh.f@husky.neu.edu>
2017-03-02 10:16:38 -0500

    RTC quicktime

--- Operation Mode/RTC_quicktime/RTC_quicktime.ino
+++ Operation Mode/RTC_quicktime/RTC_quicktime.ino
@@ -3,6 +3,9 @@
 #include "RTClib.h"
 #include <time.h>
 
+#define timeValSize 3
+
+time_t timeVal[timeValSize] = {1488447936, 1488447936 + 400, 1488447936 + 1000};
 
 enum module {
   SYSTEM
@@ -21,6 +24,16 @@ Serial.begin(9600);
 }
 
 void loop() {
+  
+  String t ="";
+  timestamp(t);
+  //Serial.println(t);
+
+  for (int i=0; i<timeValSize; i++){
+    timetest(timeVal[i]);
+  }
+
+  while (1);
 }
 
 void initRTC()
@@ -28,13 +41,15 @@ void initRTC()
   if (! rtc.begin()) {
     while (1);
   }
-  
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
+ 
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //Serial.print(DateTime(F(__DATE__), F(__TIME__)).unixtime());
   Serial.print("RTC value equals: ");
   String t ="";
   timestamp(t);
   Serial.println(t);
+  
 }
 
 void timestamp(String &timeFormat)
@@ -46,4 +61,47 @@ void timestamp(String &timeFormat)
   
 }
 
+void timetest(time_t val){
+
+  Serial.println("");
+  Serial.println("");
+  Serial.println("");
+  
+  DateTime now = rtc.now();
+  Serial.print("Inputted time: ");
+  Serial.println(val);
+  String f ="";
+  timeStampCustom(f,val);
+  Serial.print("Inputted time stamp: ");
+  Serial.println(f);
+  
+
+  time_t difference;
+
+  if (now.unixtime() > val){
+   difference = now.unixtime() - val;
+   Serial.println("The desired time is in the past");
+  }
+  else {
+    difference = val-now.unixtime();
+    Serial.println("The desired time is in the future");
+  }
+  Serial.print("Approx difference in seconds: ");
+  Serial.println(difference);
+  Serial.print("Approx difference in minutes: ");
+  Serial.println(difference/60);
+  Serial.print("Approx difference in hours: ");
+  Serial.println(difference/3600);
+  
+  
+}
+
+void timeStampCustom(String &timeFormat, time_t customTime)
+{
+    DateTime now = customTime;
+    
+    timeFormat = (String)now.year() + "/" + (String)now.month() + "/" + (String)now.day() + " " + 
+    (String)now.hour() + ":" + (String)now.minute() + ":" + (String)now.second();
+  
+}
 
